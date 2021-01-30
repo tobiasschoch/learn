cd637ecff13df4627183a26309c7c28760bd59ab
Tobias Schoch <tobias.schoch@gmail.com>
2021-01-30 17:26:46 +0100

    added console tutorial

--- update.sh
+++ update.sh
@@ -1,12 +1,33 @@
-#!/bin/sh
-# remove folder /learn
 cd /var/www/html
-rm -r learn
-# clone from github
-git clone https://github.com/tobiasschoch/learn.git
-# copy shiny-tutorial files
-cp -r /var/www/html/learn/tutorial_en/ /opt/shiny-server/samples/
-cp -r /var/www/html/learn/tutorial_de/ /opt/shiny-server/samples/
-# change
-sudo chmod -R 775 learn/
+# sudo git init	
+
+# remove the current version 
+sudo rm -r learn/  	
+
+# retrieve from github
+sudo git clone https://github.com/tobiasschoch/learn.git
+
+#-------------------------------------------------------------------------------
+# website 
+#-------------------------------------------------------------------------------
+# copy R binaries to download folder
+sudo cp /var/www/html/R_repo/R-4.0.3-win.exe /var/www/html/learn/website/download/
+sudo cp /var/www/html/R_repo/R-4.0.3.pkg /var/www/html/learn/website/download/
+
+#-------------------------------------------------------------------------------
+# tutorials
+#-------------------------------------------------------------------------------
+# copy tutorials 
+sudo cp -r /var/www/html/learn/tutorial_en /srv/shiny-server/
+sudo cp -r /var/www/html/learn/tutorial_de /srv/shiny-server/
+
+# change permissions
+sudo chmod -R 775 /srv/shiny-server/tutorial_en
+sudo chmod -R 775 /srv/shiny-server/tutorial_de
+
+# sudo R rmarkdown::run("categorical_data.Rmd") #in sudo R
+# for every tutorial...
+
+# change permissions
+sudo chmod -R 775 /srv/shiny-server/sample-apps/
 
--- website/download/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# copy binaries 
-cp /var/www/html/R_repo/R-4.0.3-win.exe /var/www/html/learn/website/download/
-
-cp /var/www/html/R_repo/R-4.0.3.pkg /var/www/html/learn/website/download/
-
-
