22e7e2a0c3271e854aacbe9b25d52dfe2efc2b23
Tobias Schoch <tobias.schoch@gmail.com>
2021-07-19 20:09:00 +0200

    dry run of tutorial on server

--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 cd /var/www/html
-# sudo git init	
+# sudo git init
 
-# remove the current version 
-sudo rm -r learn/  	
+# remove the current version
+sudo rm -r learn/
 
 # retrieve from github
 sudo git clone https://github.com/tobiasschoch/learn.git
 
 #-------------------------------------------------------------------------------
-# website 
+# website
 #-------------------------------------------------------------------------------
 # copy R binaries to download folder
 sudo mkdir /var/www/html/learn/website/download
@@ -18,27 +18,29 @@ sudo cp /var/www/html/R_repo/R-4.0.3.pkg /var/www/html/learn/website/download/
 #-------------------------------------------------------------------------------
 # tutorials
 #-------------------------------------------------------------------------------
-# copy tutorials 
+# copy tutorials
 sudo cp -r /var/www/html/learn/tutorial_en /srv/shiny-server/
 
 # allenfalls zuerst mkdir tutorial_de
-sudo cp -r /var/www/html/learn/tutorial_de /srv/shiny-server/
+# sudo cp -r /var/www/html/learn/tutorial_de /srv/shiny-server/
 
 # change permissions
 sudo chmod -R 775 /srv/shiny-server/tutorial_en
 sudo chmod -R 775 /srv/shiny-server/tutorial_de
 
-# update/ render the tutorials
-#FIXME: funktioniert nicht 
-sudo Rscript -e "setwd('/srv/shiny-server/tutorial_en/basics/')" - e "rmarkdown::run('basics.Rmd')"
-sudo Rscript -e "setwd('/srv/shiny-server/tutorial_en/console/')" - e "rmarkdown::run('console.Rmd')"
-sudo Rscript -e "setwd('/srv/shiny-server/tutorial_en/data_structures_1/')" - e "rmarkdown::run('data_structures_1.Rmd')"
-sudo Rscript -e "setwd('/srv/shiny-server/tutorial_en/intro/')" - e "rmarkdown::run('intro.Rmd')"
-sudo Rscript -e "setwd('/srv/shiny-server/tutorial_en/simple_regression/')" - e "rmarkdown::run('simple_regression.Rmd')"
+# update/ render the tutorials (using expect)
+cd /srv/shiny-server/tutorial_en
+for TUTORIAL in basics console data_structures_1 simple_regression \
+	statistical_tests
+do
+	cd $TUTORIAL
+	sudo rm $TUTORIAL.html
+	sudo expect /var/www/html/learn/run_tutorial $TUTORIAL
+	cd ..
+done
 
 # change permissions
 sudo chmod -R 775 /srv/shiny-server/tutorial_en
 
 # restart server
 systemctl restart shiny-server.service
-
